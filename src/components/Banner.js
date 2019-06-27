051c790b5fcf1cf0e67ea4041f9ac78630d96749
Brad Cas <bmcasalvieri@gmail.com>
2019-06-26 22:49:31 -0400

    centered text on page

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import styles from "../css/banner.module.css"
 export default class Banner extends Component {
   componentDidMount() {
     this.typed = new Typed(this.el, {
-      strings: ["Hi! I'm Brad.^1000", "Hi! I'm a UX-Focused Web Developer.^1000"],
+      strings: ["Hi! I'm Brad.^1000", "Hi! I'm a UX-Focused\n Web Developer.^1000"],
       typeSpeed: 50,
       backSpeed: 50,
       loop: true
@@ -14,7 +14,10 @@ export default class Banner extends Component {
   render() {
     return (
       <div className={styles.banner}>
-        <span ref={(el) => {this.el = el}} />
+        <div>
+
+          <span className={styles.bannerTyped} ref={(el) => {this.el = el}} />
+        </div>
       </div>
     )
   }
