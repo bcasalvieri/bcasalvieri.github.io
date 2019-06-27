1112e5d3ad9c0ae2d2dfe33046cc8e49ee289f37
Brad Cas <bmcasalvieri@gmail.com>
2019-06-26 23:04:47 -0400

    tweaks to banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import styles from "../css/banner.module.css"
 export default class Banner extends Component {
   componentDidMount() {
     this.typed = new Typed(this.el, {
-      strings: ["Hi! I'm Brad.^1000", "Hi! I'm a UX-Focused\n Web Developer.^1000"],
+      strings: ["Hi! I'm Brad.^1000", "I'm a UX-Focused\n Web Developer.^1000"],
       typeSpeed: 50,
       backSpeed: 50,
       loop: true
@@ -15,8 +15,7 @@ export default class Banner extends Component {
     return (
       <div className={styles.banner}>
         <div>
-
-          <span className={styles.bannerTyped} ref={(el) => {this.el = el}} />
+          <span ref={(el) => {this.el = el}} />
         </div>
       </div>
     )
