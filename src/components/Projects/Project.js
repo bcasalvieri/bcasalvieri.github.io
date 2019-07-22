54bf9b06efd914b8b484301b005b4e78c5d145b6
Brad Cas <bmcasalvieri@gmail.com>
2019-07-22 10:51:49 -0400

    fixed about image, featured works

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -2,10 +2,9 @@ import React from "react"
 import Img from "gatsby-image"
 import styles from "../../css/project.module.css"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
-import PropTypes from "prop-types"
 
 const Project = ({ project }) => {
-  const { name, slug, url, githubUrl, image } = project
+  const { name, slug, image } = project
 
   return (
     <article className={styles.project}>
@@ -17,24 +16,9 @@ const Project = ({ project }) => {
       </div>
       <div className={styles.footer}>
         <h3>{name}</h3>
-        <div className={styles.info}>
-          <div className={styles.details}>
-            <h6>URL: <a href={url}>{url}</a></h6>
-            <h6>GitHub Repo: <a href={githubUrl}>{githubUrl}</a></h6>
-          </div>
-        </div>
       </div>
     </article>
   )
-}
-
-Project.propTypes = {
-  project: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-    githubUrl: PropTypes.string.isRequired,
-  }),
 }
 
 export default Project
